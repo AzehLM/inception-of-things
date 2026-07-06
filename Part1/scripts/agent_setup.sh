8bca0ddb7dfbef955bd64441bf4dc3f0f4b72b85
Noalune12 <louanne.buisson@gmail.com>
2026-07-06 10:51:41 +0200

    feat(part1): use ansible for ssh connection and k3s configuration

--- Part1/scripts/agent_setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo apt-get update && sudo apt-get install -y curl
-TOKEN=$(ssh vagrant@192.168.56.110 sudo cat /var/lib/rancher/k3s/server/node-token)
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN sh -s - --node-ip 192.168.56.111
--- Part1/scripts/server_setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo apt-get update && sudo apt-get install -y curl
-curl -sfL https://get.k3s.io | sh -s - --node-ip 192.168.56.110 --bind-address 192.168.56.110
